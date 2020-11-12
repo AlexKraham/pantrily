567cbecd3d378f0b7773c5867d8f9da1448aeba7
Alexander Phan <alexander.phan@me.com>
2020-11-12 10:55:29 -0800

    Added stream builder for settings form to dynamically set data based on current user preference in the settings form

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:pantrily/models/brew.dart';
 import 'package:pantrily/models/pantry.dart';
+import 'package:pantrily/models/user.dart';
 
 class DatabaseService {
   final String uid;
@@ -29,8 +30,22 @@ class DatabaseService {
     }).toList();
   }
 
+  // userData from snapshot
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    return UserData(
+        uid: uid,
+        name: snapshot.data()['name'],
+        sugars: snapshot.data()['sugars'],
+        strength: snapshot.data()['strength']);
+  }
+
   // get brews stream
   Stream<List<Brew>> get brews {
     return brewCollection.snapshots().map(_brewListFromSnapshot);
+  }
+
+  // get user doc stream
+  Stream<UserData> get userData {
+    return brewCollection.doc(uid).snapshots().map(_userDataFromSnapshot);
   }
 }
